65f37f8ff82ec123e58c6ac6badd741cfb963123
Василий <naukomania@gmail.com>
2018-01-26 00:17:21 +0700

    start game development example

---
+++ space-game/script.js
@@ -0,0 +1,41 @@
+// stage
+	// width
+	// height
+	// bg
+
+	// render
+
+
+// ship
+	// properties
+	// bgImg
+	// hp
+	// direction
+
+	// functions
+	// shoot
+	// die
+	// moveLeft, moveRight, moveTop, moveBottom
+
+
+	// this.render = function() {
+	// 	// ...
+	// 	if( direction == 'right'){ 
+	// 		$(this.jq).css('background-image', 'url(/img/right-ship.jpg)');
+	// 	}
+	// }
+
+
+// meteor
+
+
+// money
+
+
+
+// var tick = function() {
+// 	stage.render();
+// 	ship.render();
+// 	meteorCollection.render();
+// 	moneyCollection.render();
+// }
